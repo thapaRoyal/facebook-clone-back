759d4e0374b1e80b2990895ab61d67c6a2580ef9
thapaRoyal <thaparoyal17@gmail.com>
2022-09-23 23:59:27 -0400

    database connection

---
+++ database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+function DbConnect() {
+  // Database connection
+  mongoose.connect(process.env.DATABASE_URL_ATLAS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log('DB connected...');
+  });
+}
+
+module.exports = DbConnect;
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
+const DbConnect = require('./database');
 
 const userRoutes = require('./routes/user');
 
 const app = express();
 app.use(cors());
 
+// routes
 app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 8000;
+
+// database
+DbConnect();
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
